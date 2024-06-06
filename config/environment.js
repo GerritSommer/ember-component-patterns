557cd402e610ffcc575af7b8aa458023985ac863
Gerrit Sommer <gerrit.sommer@outlook.com>
2024-06-06 14:00:49 +0200

    add gh deploy

--- config/environment.js
+++ config/environment.js
@@ -42,6 +42,8 @@ module.exports = function (environment) {
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
+    ENV.rootURL = '/ember-component-patterns';
+    ENV.locationType = 'hash';
   }
 
   return ENV;
