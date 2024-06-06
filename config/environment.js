531cfca3a6771ba4304a3f67db2d44f878ec26e1
Gerrit Sommer <gerrit.sommer@outlook.com>
2024-06-06 14:32:21 +0200

    fix link error

--- config/environment.js
+++ config/environment.js
@@ -43,7 +43,7 @@ module.exports = function (environment) {
   if (environment === 'production') {
     // here you can enable a production-specific feature
     ENV.rootURL = '/ember-component-patterns';
-    ENV.locationType = 'hash';
+    // ENV.locationType = 'hash';
   }
 
   return ENV;
